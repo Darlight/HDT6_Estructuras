a288de99ff97ef655118974359a276fbe940b708
Darlight <andres.perdomo09@gmail.com>
2019-03-08 21:27:13 -0600

    Carta implementada

--- Carta.java
+++ Carta.java
@@ -3,5 +3,26 @@ Autores:    Mario Perdomo 18029
             Josue Sagastume 18173
 
 Fecha: 8 de Marzo de 2019
-Proposito: Factory que instancia el tipo de Mapa a utilizar
+Proposito: Clase de las cartas
  */
+public class Carta{
+
+    private String nombre;
+    private String tipo;
+
+    public Card(String nombre, String tipo){
+        this.nombre = name;
+        this.tipo = type;
+    }
+
+    public String getTipo(){
+        return this.tipo;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String toString() {
+        return "Nombre de la carta: " + nombre + "/ Tipo: " + tipo;
+    }
+}
